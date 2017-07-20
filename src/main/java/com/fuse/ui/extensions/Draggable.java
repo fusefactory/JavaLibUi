cd6c0a7cfac5d8a9fd9ffce522dbf1bc710ec1c1
shortnotion (mark) <shortnotion@gmail.com>
2017-07-20 17:46:52 +0200

    startEvent, endEvent and getOffset method for Draggable extension

--- src/main/java/com/fuse/ui/extensions/Draggable.java
+++ src/main/java/com/fuse/ui/extensions/Draggable.java
@@ -2,21 +2,31 @@ package com.fuse.ui.extensions;
 
 import processing.core.PVector;
 
+import com.fuse.utils.Event;
 import com.fuse.ui.Node;
 import com.fuse.ui.TouchEvent;
 
 public class Draggable extends ExtensionBase {
   private PVector originalNodePosition = null;
 
+  public Event<Draggable> startEvent;
+  public Event<Draggable> endEvent;
+
+  public Draggable(){
+    startEvent = new Event<>();
+    endEvent = new Event<>();
+  }
+
   @Override public void enable(Node newNode){
     super.enable(newNode);
 
-    node.touchDownEvent.addListener((TouchEvent event) -> {
-      originalNodePosition = getNode().getPosition();
-    }, this);
-
     node.touchMoveEvent.addListener((TouchEvent event) -> {
-      if(originalNodePosition == null) return; // happens sometimes with race conditions
+      if(originalNodePosition == null){
+        originalNodePosition = getNode().getPosition();
+        // TODO; min offset before officialy start dragging?
+        startEvent.trigger(this);
+      }
+
       // logger.warning("offset: "+event.offset().toString()+", orig pos: "+originalNodePosition.toString());
       getNode().setPosition(PVector.add(event.offset(), originalNodePosition));
     }, this);
@@ -25,12 +35,12 @@ public class Draggable extends ExtensionBase {
       if(originalNodePosition == null) return; // happens sometimes with race conditions
       getNode().setPosition(PVector.add(event.offset(), originalNodePosition));
       originalNodePosition = null;
+      endEvent.trigger(this);
     }, this);
   }
 
   @Override public void disable(){
     super.disable();
-    node.touchDownEvent.removeListeners(this);
     node.touchMoveEvent.removeListeners(this);
     node.touchUpEvent.removeListeners(this);
   }
@@ -49,5 +59,12 @@ public class Draggable extends ExtensionBase {
       if(Draggable.class.isInstance(n.getExtensions().get(i))){
         n.stopUsing(n.getExtensions().get(i));
       }
+  }
+
+  public PVector getOffset(){
+    if(originalNodePosition == null || node == null)
+      return new PVector(0.0f,0.0f,0.0f);
+
+    return node.getPosition().sub(originalNodePosition);
   }
 }
--- src/test/java/com/fuse/ui/extensions/DraggableTest.java
+++ src/test/java/com/fuse/ui/extensions/DraggableTest.java
@@ -82,4 +82,59 @@ public class DraggableTest {
     tm.touchUp(0, new PVector(55,55,0));
     assertEquals(n.getPosition(), new PVector(10,10,0));
   }
+
+  @Test public void getOffset(){
+    // create scene
+    Node scene = new Node();
+    Node n = new Node();
+    n.setPosition(10, 10);
+    n.setSize(100, 100);
+    scene.addChild(n);
+
+    // create touch manager that acts on this scene
+    TouchManager tm = new TouchManager(scene);
+    // enable draggable on node
+    Draggable d = Draggable.enableFor(n);
+
+    assertEquals(d.getOffset(), new PVector(0.0f, 0.0f, 0.0f));
+    tm.touchDown(0, new PVector(20,20,0));
+    assertEquals(d.getOffset(), new PVector(0.0f, 0.0f, 0.0f));
+    tm.touchMove(0, new PVector(30,30,0));
+    assertEquals(d.getOffset(), new PVector(10.0f, 10.0f, 0.0f));
+    tm.touchMove(0, new PVector(15,25,0));
+    assertEquals(d.getOffset(), new PVector(-5.0f, 5.0f, 0.0f));
+  }
+
+  @Test public void events(){
+    // create scene
+    Node scene = new Node();
+    Node n = new Node();
+    n.setPosition(10, 10);
+    n.setSize(100, 100);
+    scene.addChild(n);
+
+    // create touch manager that acts on this scene
+    TouchManager tm = new TouchManager(scene);
+    // enable draggable on node
+    Draggable d = Draggable.enableFor(n);
+
+    d.startEvent.enableHistory();
+    d.endEvent.enableHistory();
+
+    assertEquals(d.startEvent.getHistory().size(), 0);
+    assertEquals(d.endEvent.getHistory().size(), 0);
+
+    tm.touchDown(0, new PVector(20,20,0));
+    assertEquals(d.startEvent.getHistory().size(), 0);
+    assertEquals(d.endEvent.getHistory().size(), 0);
+    tm.touchMove(0, new PVector(30,30,0));
+    assertEquals(d.startEvent.getHistory().size(), 1);
+    assertEquals(d.endEvent.getHistory().size(), 0);
+    tm.touchMove(0, new PVector(50,30,0));
+    assertEquals(d.startEvent.getHistory().size(), 1);
+    assertEquals(d.endEvent.getHistory().size(), 0);
+    tm.touchUp(0, new PVector(15,25,0));
+    assertEquals(d.startEvent.getHistory().size(), 1);
+    assertEquals(d.endEvent.getHistory().size(), 1);
+  }
 }
