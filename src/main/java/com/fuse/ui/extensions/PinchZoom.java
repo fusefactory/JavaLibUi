527af7f3cf21dc897230250544314bbd389dce29
shortnotion (mark) <shortnotion@gmail.com>
2017-07-21 22:39:36 +0200

    pinch-zoom improvements, not optimal yet

--- src/main/java/com/fuse/ui/extensions/PinchZoom.java
+++ src/main/java/com/fuse/ui/extensions/PinchZoom.java
@@ -8,10 +8,17 @@ import com.fuse.ui.TouchEvent;
 
 public class PinchZoom extends ExtensionBase {
 
+  public enum ZoomMode {
+    SIZE,
+    SCALE
+  }
+
   private TouchEvent touchEvent1 = null;
   private TouchEvent touchEvent2 = null;
   /** This will hold the center of the _start_ of the two touches in global coordinate space */
   private PVector originalNodePositionGlobal = null;
+  private PVector originalNodeSize = null;
+  private ZoomMode zoomMode = ZoomMode.SIZE;
 
   public PinchZoom(){
   }
@@ -37,6 +44,7 @@ public class PinchZoom extends ExtensionBase {
       if(touchEvent2 == null){
         touchEvent2 = event;
         originalNodePositionGlobal = node.getGlobalPosition();
+        originalNodeSize = node.getSize();
       }
 
       // already two touches; ignore any other touches
@@ -60,9 +68,19 @@ public class PinchZoom extends ExtensionBase {
       PVector scaler = getGlobalPinchScale();
       PVector translater = getGlobalPinchTranslate();
       // System.out.println("PinchZoom scale: "+scaler.toString());
-      getNode().setScale(scaler);
       getNode().setGlobalPosition(translater.add(originalNodePositionGlobal));
 
+      if(zoomMode == ZoomMode.SIZE){
+        PVector newSize = new PVector(
+          originalNodeSize.x * scaler.x,
+          originalNodeSize.y * scaler.y,
+          originalNodeSize.z * scaler.z);
+        getNode().setSize(newSize);
+      }
+
+      if(zoomMode == ZoomMode.SCALE){
+        getNode().setScale(scaler);
+      }
     }, this);
   }
 
@@ -73,20 +91,6 @@ public class PinchZoom extends ExtensionBase {
 
   boolean isActive(){
     return touchEvent1 != null && touchEvent2 != null;
-  }
-
-  public PVector getGlobalStartCenter(){
-    if(!isActive()) return null;
-
-    PVector delta = touchEvent2.startPosition.copy().sub(touchEvent1.startPosition);
-    return PVector.add(delta.mult(0.5f), touchEvent1.startPosition);
-  }
-
-  public PVector getGlobalCurrentCenter(){
-    if(!isActive()) return null;
-
-    PVector delta = touchEvent2.position.copy().sub(touchEvent1.position);
-    return PVector.add(delta.mult(0.5f), touchEvent1.position);
   }
 
   /** @return PVector Distance between the start-points of the two touches */
@@ -106,21 +110,29 @@ public class PinchZoom extends ExtensionBase {
     PVector start = getGlobalStartDelta();
     PVector current = getGlobalCurrentDelta();
     return new PVector(
-      start.x == 0.0f ? 1.0f : current.x / start.x,
-      start.y == 0.0f ? 1.0f : current.y / start.y,
-      start.z == 0.0f ? 1.0f : current.z / start.z);
+      Math.abs(start.x == 0.0f ? 1.0f : current.x / start.x),
+      Math.abs(start.y == 0.0f ? 1.0f : current.y / start.y),
+      Math.abs(start.z == 0.0f ? 1.0f : current.z / start.z));
   }
 
   public PVector getGlobalPinchTranslate(){
     if(!isActive()) return new PVector();
 
+
+    // start center of two touches
+    PVector delta = touchEvent2.startPosition.copy().sub(touchEvent1.startPosition);
+    PVector startCenter = PVector.add(delta.mult(0.5f), touchEvent1.startPosition);
+
+    PVector currentDelta = touchEvent2.position.copy().sub(touchEvent1.position);
+    PVector currentCenter = PVector.add(currentDelta.mult(0.5f), touchEvent1.position);
+
     PVector scale = getGlobalPinchScale();
-    PVector touchOffset = touchEvent1.offset().copy();
+    PVector touchOffset = currentCenter.sub(startCenter).add(touchEvent1.offset());
 
     return new PVector(
-      scale.x*touchOffset.x,
-      scale.y*touchOffset.y,
-      scale.z*touchOffset.z);
+      -Math.abs(scale.x*touchOffset.x),
+      -Math.abs(scale.y*touchOffset.y),
+      -Math.abs(scale.z*touchOffset.z));
   }
 
   public static PinchZoom enableFor(Node n){
