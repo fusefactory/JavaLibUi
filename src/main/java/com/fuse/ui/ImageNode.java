2a4c8ad6fa5c6f4ecbd0db7d797fa3b28c0cd4c4
shortnotion (mark) <shortnotion@gmail.com>
2017-07-27 15:02:31 +0200

    FIT_CENTERED mode for ImageNode

--- src/main/java/com/fuse/ui/ImageNode.java
+++ src/main/java/com/fuse/ui/ImageNode.java
@@ -10,13 +10,15 @@ public class ImageNode extends Node {
   public enum Mode {
     NORMAL, // image rendered at original size at Node's origin (0,0) position
     CENTER, // image rendered at original size centered inside the node
-    FIT
+    FIT,    // image is stretched/squeezed into exactly this node's dimensions
+    FIT_CENTERED // image is stretched/squeezed to fit inside the node, but aspect ratio is respected
   }
 
   private PImage image;
   private Mode mode;
   private boolean autoResizeToImage = false;
   private Integer tintColor = null;
+  private PVector fitCenteredSize = null;
 
   private void _init(){
     image = null;
@@ -41,16 +43,32 @@ public class ImageNode extends Node {
     if(tintColor != null)
       pg.tint(tintColor);
 
-    if(mode == Mode.NORMAL){
-      pg.image(image, 0.0f, 0.0f);
-    } else if(mode == Mode.CENTER){
-      PVector pos = PVector.mult(getSize(), 0.5f);
-      pg.imageMode(PApplet.CENTER);
-      pg.image(image, pos.x, pos.y);
-      pg.imageMode(PApplet.CORNERS); // restore default
-    } else if(mode == Mode.FIT){
-      pg.imageMode(PApplet.CORNERS);
-      pg.image(image, 0.0f, 0.0f, getSize().x, getSize().y);
+    switch(mode){
+      case NORMAL : {
+        pg.image(image, 0.0f, 0.0f);
+        break;
+      }
+      case CENTER : {
+        PVector pos = PVector.mult(getSize(), 0.5f);
+        pg.imageMode(PApplet.CENTER);
+        pg.image(image, pos.x, pos.y);
+        pg.imageMode(PApplet.CORNERS); // restore default
+        break;
+      }
+      case FIT : {
+        pg.imageMode(PApplet.CORNERS);
+        pg.image(image, 0.0f, 0.0f, getSize().x, getSize().y);
+        break;
+      }
+      case FIT_CENTERED : {
+        // "cache" the centered fit size
+        if(fitCenteredSize == null)
+          fitCenteredSize = calculateFitCenteredSize();
+        PVector pos = PVector.mult(getSize(), 0.5f);
+        pg.imageMode(PApplet.CENTER);
+        pg.image(image, pos.x, pos.y, fitCenteredSize.x, fitCenteredSize.y);
+        pg.imageMode(PApplet.CORNERS); // restore default
+      }
     }
 
     if(tintColor != null)
@@ -84,4 +102,20 @@ public class ImageNode extends Node {
 
   public void setTint(Integer clr){ tintColor = clr; }
   public Integer getTint(){ return tintColor; }
+
+  private PVector calculateFitCenteredSize(){
+    if(image == null) return new PVector(0.0f,0.0f,0.0f);
+
+    float w = getSize().x / image.width;
+    float h = getSize().y / image.height;
+    if(w > h){
+      w = h * image.width;
+      h = h * image.height;
+    } else {
+      h = w * image.height;
+      w = w * image.width;
+    }
+
+    return new PVector(w,h,0.0f);
+  }
 }
