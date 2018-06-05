e3df7f3e4f81680139dafee7ba4f7efc05ce8a7a
mark <mark.korput@gmail.com>
2018-06-05 10:39:36 +0200

    start new example app for chromium test

---
+++ example-chromium/src/main/java/com/fuse/ui/example/App.java
@@ -0,0 +1,109 @@
+package com.fuse.ui.example;
+
+import java.util.logging.*;
+import processing.core.PApplet;
+import processing.core.PGraphics;
+import processing.core.PVector;
+
+import com.fuse.ui.TouchManager;
+import com.fuse.ui.Node;
+
+public class App extends PApplet {
+  private Logger logger;
+  private PApplet papplet;
+  private PGraphics pg;
+  private float timeBetweenFrames;
+  private boolean bDrawDebug;
+
+  private TouchManager touchManager;
+  private Node sceneNode;
+
+  public static void main( String[] args )
+  {
+    PApplet.main("com.fuse.ui.example.App");
+  }
+
+  public App(){
+    super();
+    logger = Logger.getLogger("");//App.class.getName());
+    logger.setLevel(Level.ALL);
+    papplet = this;
+  }
+
+  public void settings(){
+    size(800, 600, P3D);
+  }
+
+  public void setup(){
+
+    // configure processing applet
+    papplet.frameRate(30.0f);
+    timeBetweenFrames = 1.0f / papplet.frameRate;
+    bDrawDebug = false;
+
+    // initialize UI system (framebuffer/scene/touch)
+    pg = papplet.createGraphics(papplet.width, papplet.height, P3D);
+    Node.setPGraphics(pg);
+
+    sceneNode = new Node();
+    sceneNode.setSize(papplet.width, papplet.height);
+
+    touchManager = new TouchManager();
+    touchManager.setNode(sceneNode);
+
+    // create scene content
+
+  }
+
+  private void update(float dt){
+    // run update on all UI nodes
+    sceneNode.updateSubtree(dt);
+  }
+
+  public void draw(){
+    // OF-style; first update all "data" before rendering
+    update(timeBetweenFrames);
+
+    papplet.background(0);
+    papplet.clear();
+
+    // UI (2D)
+    pg.beginDraw();
+    {
+      pg.clear();
+      sceneNode.render();
+
+      if(bDrawDebug){
+        sceneNode.renderDebug();
+        touchManager.drawActiveTouches();
+      }
+    }
+    pg.endDraw();
+
+    papplet.image(pg, 0f,0f);
+  }
+
+  public void mousePressed(){
+    // logger.finest("mousePressed: " + Integer.toString(mouseX) + ", " + Integer.toString(mouseY));
+    touchManager.touchDown(0, new PVector(mouseX, mouseY, 0f));
+  }
+
+  public void mouseDragged(){
+    // logger.finest("mouseDragged: " + Integer.toString(mouseX) + ", " + Integer.toString(mouseY));
+    touchManager.touchMove(0, new PVector(mouseX, mouseY, 0f));
+  }
+
+  public void mouseReleased(){
+    // logger.finest("mouseReleased: " + Integer.toString(mouseX) + ", " + Integer.toString(mouseY));
+    touchManager.touchUp(0, new PVector(mouseX, mouseY, 0f));
+  }
+
+  public void keyPressed(){
+    switch(key){
+      case 'd': {
+        bDrawDebug = !bDrawDebug;
+        return;
+      }
+    }
+  }
+}
