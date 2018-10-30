42b69b4a14c75332b949be2b6e9afc3e5ae5eebf
mark <mark.korput@gmail.com>
2018-10-31 00:49:22 +0100

    multitouch example app grab touch id from native mouse events (needs testing with actual touch interfaces

--- example-multitouch/src/main/java/com/fuse/ui/example/App.java
+++ example-multitouch/src/main/java/com/fuse/ui/example/App.java
@@ -1,19 +1,15 @@
 package com.fuse.ui.example;
 
 import java.util.logging.*;
+import java.util.LinkedList;
 import processing.core.PApplet;
 import processing.core.PGraphics;
 import processing.core.PVector;
+import processing.event.MouseEvent;
 
 import com.fuse.ui.TouchManager;
 import com.fuse.ui.Node;
-import com.fuse.ui.RectNode;
 import com.fuse.ui.TextNode;
-import com.fuse.ui.extensions.Draggable;
-import com.fuse.ui.extensions.Swiper;
-import com.fuse.ui.extensions.PinchZoom;
-import com.fuse.ui.extensions.SmoothScroll;
-import com.fuse.ui.extensions.Constrain;
 
 public class App extends PApplet {
   Logger logger;
@@ -24,6 +20,8 @@ public class App extends PApplet {
 
   private TouchManager touchManager;
   private Node sceneNode;
+  private TextNode logNode;
+  private LinkedList<String> logLines = new LinkedList<String>();
 
   public static void main( String[] args )
   {
@@ -54,6 +52,24 @@ public class App extends PApplet {
 
     touchManager = new TouchManager();
     touchManager.setNode(sceneNode);
+
+    this.logNode = new TextNode();
+    this.logNode.setPosition(5, 5);
+    this.logNode.setSize(PVector.sub(this.sceneNode.getSize(), new PVector(10,10)));
+    this.logNode.setTextSize(14);
+    this.sceneNode.addChild(this.logNode);
+
+    touchManager.touchDownEvent.addListener((evt) ->this.log("[touchDownEvent] " + evt.toString()));
+    touchManager.touchMoveEvent.addListener((evt) ->this.log("[touchMoveEvent] " + evt.toString()));
+    touchManager.touchUpEvent.addListener((evt) ->this.log("[touchUpEvent] " + evt.toString()));
+    // touchManager.touchEvent.addListener((evt) -> this.log("[touchEvent] " + evt.toString()));
+  }
+
+  private void log(String line) {
+    // System.out.println("touchevent: "+evt.toString());
+    while (this.logLines.size() >= 25) this.logLines.removeFirst();
+    this.logLines.add(line);
+    this.logNode.setText(String.join("\n", this.logLines));
   }
 
   private void update(float dt){
@@ -84,20 +100,47 @@ public class App extends PApplet {
     papplet.image(pg, 0f,0f);
   }
 
-  public void mousePressed(){
-    // logger.finest("mousePressed: " + Integer.toString(mouseX) + ", " + Integer.toString(mouseY));
-    touchManager.touchDown(0, new PVector(mouseX, mouseY, 0f));
+  @Override
+  public void mousePressed(MouseEvent event) {
+    // touchManager.touchDown(0, new PVector(mouseX, mouseY, 0f));
+    if (event.getNative() instanceof com.jogamp.newt.event.MouseEvent) {
+      com.jogamp.newt.event.MouseEvent nativeEvent = (com.jogamp.newt.event.MouseEvent) event.getNative();
+      touchManager.touchDown(nativeEvent.getPointerId(0), new PVector(mouseX, mouseY, 0f));
+    }
   }
 
-  public void mouseDragged(){
-    // logger.finest("mouseDragged: " + Integer.toString(mouseX) + ", " + Integer.toString(mouseY));
-    touchManager.touchMove(0, new PVector(mouseX, mouseY, 0f));
+  @Override
+  public void mouseDragged(MouseEvent event) {
+    // touchManager.touchMove(0, new PVector(mouseX, mouseY, 0f));
+    if (event.getNative() instanceof com.jogamp.newt.event.MouseEvent) {
+      com.jogamp.newt.event.MouseEvent nativeEvent = (com.jogamp.newt.event.MouseEvent) event.getNative();
+      touchManager.touchMove(nativeEvent.getPointerId(0), new PVector(mouseX, mouseY, 0f));
+    }
   }
 
-  public void mouseReleased(){
-    // logger.finest("mouseReleased: " + Integer.toString(mouseX) + ", " + Integer.toString(mouseY));
-    touchManager.touchUp(0, new PVector(mouseX, mouseY, 0f));
+  @Override
+  public void mouseReleased(MouseEvent event) {
+    // touchManager.touchUp(0, new PVector(mouseX, mouseY, 0f));
+    if (event.getNative() instanceof com.jogamp.newt.event.MouseEvent) {
+      com.jogamp.newt.event.MouseEvent nativeEvent = (com.jogamp.newt.event.MouseEvent) event.getNative();
+      touchManager.touchUp(nativeEvent.getPointerId(0), new PVector(mouseX, mouseY, 0f));
+    }
   }
+
+//  public void mousePressed(){
+//    // logger.finest("mousePressed: " + Integer.toString(mouseX) + ", " + Integer.toString(mouseY));
+//    touchManager.touchDown(0, new PVector(mouseX, mouseY, 0f));
+//  }
+//
+//  public void mouseDragged(){
+//    // logger.finest("mouseDragged: " + Integer.toString(mouseX) + ", " + Integer.toString(mouseY));
+//    touchManager.touchMove(0, new PVector(mouseX, mouseY, 0f));
+//  }
+//
+//  public void mouseReleased(){
+//    // logger.finest("mouseReleased: " + Integer.toString(mouseX) + ", " + Integer.toString(mouseY));
+//    touchManager.touchUp(0, new PVector(mouseX, mouseY, 0f));
+//  }
 
   public void keyPressed(){
     switch(key){
